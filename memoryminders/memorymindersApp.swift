d4f7cfdd659ec17beb52ae5855a3d2b8fdd906f0
brandonjmarquez <60440397+brandonjmarquez@users.noreply.github.com>
2023-07-03 09:24:14 -0500

    Initial Commit

---
+++ memoryminders/memorymindersApp.swift
@@ -0,0 +1,17 @@
+//
+//  memorymindersApp.swift
+//  memoryminders
+//
+//  Created by Brandon Marquez on 7/3/23.
+//
+
+import SwiftUI
+
+@main
+struct memorymindersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
